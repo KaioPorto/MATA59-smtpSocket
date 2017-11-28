5ada49aef3bd80e00dd3cc518d004c295617e187
kaio.rodrigo <kaio.rodrigo@braquiossauro>
2017-11-28 12:12:53 -0300

    adding some fixes

--- smtpClient.py
+++ smtpClient.py
@@ -4,12 +4,26 @@ import argparse
 import socket
 
 #parsing the from, to and msg
+parser = argparse.ArgumentParser(description="simple smtp client to send messages"
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-s", "--server", help="mailserver whom gonna receive the message", nargs="*")
 
-def connect(server, port=25):
+
+
+
+def connect((server, port=25)):
     try:
-        sock = socket.create_connection((server, port))
+	sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect(server, port)
     except:
         print "Unable to connect"
+
+    recv = sock.recv(1024)
+    recv = recv.decode()
+    if (recv[:3] != '220':
+	print("220 reply not received. Not okay.")
+
+#    retrun True
 
 def send_message(msg, _from, to, subject):
 
@@ -25,4 +39,9 @@ def send_message(msg, _from, to, subject):
     sock.shutdown(socket.SHUT_RDWR)
     sock.close()
 
+args = parser.parse_args()
+server = args.server
+
+mailserver = (server, port)
+connect(mailserver)
 
